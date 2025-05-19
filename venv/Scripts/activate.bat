d84f3b131bc41c97e3c07433bc3d8b1ca2d55c36
tr-zisun34 <bsse1434@iit.du.ac.bd>
2025-05-19 10:36:03 +0600

    push error committed

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Zisun\Desktop\LAB-3-2\AI\gomoku\venv
+set VIRTUAL_ENV=C:\Users\Zisun\Desktop\GOMOKU..ft-Beta_Psi_Kappa\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
